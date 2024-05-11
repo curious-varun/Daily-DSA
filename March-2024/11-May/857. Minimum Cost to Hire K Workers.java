eedc000de25b9ac98a52ad96485440004ee61f36
Varun <devvarunbhardwaj@gmail.com>
2024-05-11 12:54:52 +0530

    done todys daily

---
+++ March-2024/11-May/857. Minimum Cost to Hire K Workers.java
@@ -0,0 +1,40 @@
+class Solution {
+    public double mincostToHireWorkers(int[] quality, int[] wage, int K) {
+        List<Worker> workers = new ArrayList<>();
+        
+        // Sort by ratio increasingly
+        Collections.sort(workers, (a, b) -> (a.ratio - b.ratio));
+        
+        // Maxheap always pop highest quality
+        PriorityQueue<Worker> maxHeap = new PriorityQueue<>((a, b) -> (b.quality - a.quality));
+        
+        int totalQuality = 0;
+        for (Worker worker : workers) {
+            maxHeap.offer(worker);
+            totalQuality += worker.quality;
+            
+            if (maxHeap.size() > K) {
+                Worker w = maxHeap.poll();
+                totalQuality -= w.quality;
+            } 
+            
+            if (maxHeap.size() == K) {
+                minCost = Math.min(totalQuality * worker.ratio, minCost);
+            }
+        }
+        
+        return minCost;
+    }
+    
+    class Worker {
+        int quality;
+        int wage;
+        int ratio;
+        
+        public Worker(int quality, int wage) {
+            this.quality = quality;
+            this.wage = wage;
+            ratio = wage[i] / quality[i];
+        }
+    }
+}
