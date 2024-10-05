e9aa06b0d2322bf1c50abb783dacaaa77ce156ba
Varun <devvarunbhardwaj@gmail.com>
2024-10-05 23:55:47 +0530

    -------------------

--- March-2024/two.java
+++
@@ -1,8 +0,0 @@
-public class two { 
-
-  public static void main(String[] args) {
-    
-    int a = 9 ;
-
-  }
-}
--- sept-2024/25-sept/one.py
+++
@@ -1,9 +0,0 @@
-class Solution:
-    def lexicalOrder(self, n: int) -> List[int]:
-        k=list(range(1,n+1))
-        def sor(n):
-            n=str(n)
-            return n[0:]
-        k.sort(key=sor)
-        return k
-        
