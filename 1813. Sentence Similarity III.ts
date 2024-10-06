3d4586545aad25850a7bca2d6f7ff192953482fc
Varun <devvarunbhardwaj@gmail.com>
2024-10-06 18:05:16 +0530

    : :

---
+++ 1813. Sentence Similarity III.ts
@@ -0,0 +1,18 @@
+function areSentencesSimilar(sentence1: string, sentence2: string): boolean {
+  const longSentence = sentence1.length >= sentence2.length ? sentence1 : sentence2;
+  const shortSentence = sentence1.length < sentence2.length ? sentence1 : sentence2;
+
+  if (longSentence === shortSentence) return true;
+
+  const splitedShortSentence = shortSentence.split(" ");
+  const wordCount = splitedShortSentence.length;
+
+  for (let i = 0; i <= wordCount; i++) {
+    const preSentence = splitedShortSentence.slice(0, i).join(' ');
+    const postSentence = splitedShortSentence.slice(i).join(' ');
+
+    if (longSentence.startsWith(preSentence ? preSentence + ' ' : '') && longSentence.endsWith(postSentence ? ' ' + postSentence : '')) return true;
+  }
+
+  return false;
+}
