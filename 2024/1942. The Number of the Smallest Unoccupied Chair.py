6082a83302a1ee7ab4599ae500592f2bdf8bed1a
Varun <devvarunbhardwaj@gmail.com>
2024-10-11 15:02:30 +0530

    : :

---
+++ 2024/1942. The Number of the Smallest Unoccupied Chair.py
@@ -0,0 +1,26 @@
+import heapq
+class Solution:
+    def smallestChair(self, times: List[List[int]], targetFriend: int) -> int:
+        n = len(times)
+        indices = [idx for idx in range(n)]
+
+        indices.sort(key = lambda idx : times[idx][0])
+
+        available_chairs = [chair for chair in range(n)]
+        filled_chairs = [] #(l,chair)
+
+        for idx in indices:
+            a, l = times[idx]
+
+            while filled_chairs and filled_chairs[0][0] <= a:
+                leave, chair = heapq.heappop(filled_chairs)
+                heapq.heappush(available_chairs, chair)
+
+            chair = heapq.heappop(available_chairs)
+
+            if idx == targetFriend:
+                return chair
+
+            heapq.heappush(filled_chairs,[l,chair])
+
+        
