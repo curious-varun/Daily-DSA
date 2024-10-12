4794c1a0bad7e82c7d1c626bf5472d3d511b25f6
Varun <devvarunbhardwaj@gmail.com>
2024-10-12 23:24:18 +0530

    : :

---
+++ 2024/2406. Divide Intervals Into Minimum Number of Groups.py
@@ -0,0 +1,9 @@
+class Solution:
+    def minGroups(self, intervals: List[List[int]]) -> int:
+        intervals = sorted(intervals, key = lambda x : x[0])
+        heap = []
+        for x, y in intervals:
+            if heap and x > heap[0]:
+                heapq.heappop(heap)
+            heapq.heappush(heap, y)
+        return len(heap)
